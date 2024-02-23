3125c028605305bf042fe5a45c44ab31ca86562c
roeintheglasses <hrkjangir@gmail.com>
2024-02-23 17:22:45 +0530

    Minor linting changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-
 // Import the native module. On web, it will be resolved to ExpoWallpaperManager.web.ts
 // and on native platforms to ExpoWallpaperManager.ts
-import ExpoWallpaperManagerModule from './ExpoWallpaperManagerModule';
-
+import ExpoWallpaperManagerModule from "./ExpoWallpaperManagerModule";
 
 export function hello(): string {
   return ExpoWallpaperManagerModule.hello();
@@ -11,4 +9,3 @@ export function hello(): string {
 export function setWallpaper(options): string {
   return ExpoWallpaperManagerModule.setWallpaper(options);
 }
-
